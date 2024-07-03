0f552640ae0ec76b70c61c0c60d5c8a882a7a00f
Javicorporation <krchipullajavier@gmail.com>
2024-07-03 16:46:16 -0500

    clase 3

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,10 +1,10 @@
-let numeroSecret = 6;
+let numeroSecret = Math.floor(Math.random()*100);
 let numeroDeUsuario = 0;
 let intentos = 1;
 let palabraVes = " vez"
 let maximoIntento = 3;
 
-alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+alert("¡Bienvenida y bienvenido a nuestro sitio web! "+numeroSecret);
 
 /*
 let numeroDeVentas = 50;
@@ -17,14 +17,15 @@ if(edad > 18){
     alert("¡Puedes obtener tu licencia de conducir!");
 }else{
     alert("vete de aqui niño.")
-}*/
+}
+*/
 
 while(numeroDeUsuario != numeroSecret ){
     numeroDeUsuario = prompt("Escribe un número: ");
     console.log(numeroDeUsuario);
 
     if (numeroDeUsuario == numeroSecret) {
-        alert(`Acertaste el numero es: ${numeroDeUsuario}, el numero de intentos fue${intentos} ${intentos == 1 ? " vez" : " veces"}`);
+        alert(`Acertaste el numero es: ${numeroDeUsuario}, el numero de intentos fue ${intentos} ${intentos == 1 ? " vez" : " veces"}`);
     }else{
         if(numeroDeUsuario < numeroSecret){
             alert("el numero secreto es mayor")
@@ -39,3 +40,26 @@ while(numeroDeUsuario != numeroSecret ){
     }
 }
 
+/*
+
+console.log("Bienvenido");
+let nombre = "javier";
+console.log(nombre);
+alert("Tu nombre es "+nombre);
+
+let leng = prompt("¿Cuál es el lenguaje de programación que más te gusta?");
+console.log(leng);
+
+let val1 = 9;
+let val2 = 2;
+let resultado = val1+val2;
+console.log("la suma es: "+resultado);
+
+let edad = prompt("Escribe tu edad: ");
+if( edad >= 18){
+    alert("es mayor de edad");
+
+}else{
+    alert("no es mayor de edad");
+}
+    */
