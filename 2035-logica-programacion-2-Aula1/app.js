56d873918db8606c4e6a02c05639c94819af2558
Javicorporation <krchipullajavier@gmail.com>
2024-07-05 16:19:49 -0500

    JavaScript usa eventos los cuales pueden ser muy utiles en diversas areas de programacion a las cuales se les puede asignar finciones

---
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.textContent = "Juego de número secreto";
+
+let parrafo = document.querySelector('p')
+parrafo.textContent = "Escribe un número del 1 al 10";
+
+function intentoDeUsuario(){
+    alert("clic desde del boton");
+}
