ed2dac2319310d217919fc738e7e0a1cf02d0ebb
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-03 16:12:06 -0500

    - modificadores de acceso - uno de métodos para crear el acceso a datos

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,23 +1,29 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula p1 = new Pelicula();
-        p1.nombre = "Pelicula de terror";
-        p1.fechaLanzamiento = 2001;
-        p1.duracionEnMinutos = 120;
+        p1.setNombre( "Pelicula de terror") ;
+        p1.setFechaLanzamiento(2001);
+        p1.setDuracionEnMinutos(120);
         p1.MuestraFichaTecnica();
         p1.evaaluar(10);
-        p1.evaaluar(10);
-        System.out.println(p1.sumaDeLaevaluaciones);
-        System.out.println(p1.totalDeEvaluaciones);
+
+        System.out.println(p1.getSumaDeLaevaluaciones());
+        System.out.println(p1.getTotalDeEvaluaciones());
         System.out.println(p1.calcularMedia());
 
 
         System.out.println();
         Pelicula p2 = new Pelicula();
-        p2.nombre = "Pelicula de comedia";
-        p2.fechaLanzamiento = 2009;
-        p2.duracionEnMinutos = 160;
+        p2.setNombre("Pelicula de comedia");
+        p2.setFechaLanzamiento(2009);
+        p2.setDuracionEnMinutos(160);
         p2.MuestraFichaTecnica();
+        p2.evaaluar(10);
+        System.out.println(p2.getSumaDeLaevaluaciones());
+        System.out.println(p2.getTotalDeEvaluaciones());
+        System.out.println(p2.calcularMedia());
 
     }
 }
